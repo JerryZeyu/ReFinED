3662465b907bb92665b7897a0ea7a6265d991291
JerryZeyu <zzy19951013@gmail.com>
2023-02-01 15:56:43 -0700

    update codes

--- src/refined/offline_data_generation/generate_descriptions_tensor_UMLS.py
+++ src/refined/offline_data_generation/generate_descriptions_tensor_UMLS.py
@@ -4,8 +4,16 @@ import torch
 from tqdm.auto import tqdm
 from transformers import AutoTokenizer
 from typing import Dict
-
+import sys
 import ujson
+
+def pickle_dump_large_file(obj, filepath):
+    max_bytes = 2**31 - 1
+    bytes_out = pickle.dumps(obj)
+    n_bytes = sys.getsizeof(bytes_out)
+    with open(filepath, 'wb') as f_out:
+        for idx in range(0, n_bytes, max_bytes):
+            f_out.write(bytes_out[idx:idx + max_bytes])
 def pickle_load_large_file(filepath):
     max_bytes = 2**31 - 1
     input_size = os.path.getsize(filepath)
@@ -36,7 +44,7 @@ def create_description_tensor(output_path: str, umlsID_to_idx_filename: str, des
     umlsIDs = list(labels.keys())
     descriptions = pickle_load_large_file(desc_filename)
     umlsID_to_idx = load_umlsID_to_idx(umlsID_to_idx_filename)
-
+    pickle_dump_large_file(umlsID_to_idx, os.path.join(OUTPUT_PATH, 'umlsID_to_idx.pkl'))
 
     # TODO: check no extra [SEP] tokens between label and description or extra [CLS] or [SEP] at end
     tokenizer = AutoTokenizer.from_pretrained(tokeniser, use_fast=True, add_prefix_space=False)
--- src/refined/resource_management/data_lookups.py
+++ src/refined/resource_management/data_lookups.py
@@ -12,7 +12,14 @@ from refined.resource_management.aws import S3Manager
 from refined.resource_management.lmdb_wrapper import LmdbImmutableDict
 from refined.resource_management.loaders import load_human_qcode
 import os
-
+import sys
+def pickle_dump_large_file(obj, filepath):
+    max_bytes = 2**31 - 1
+    bytes_out = pickle.dumps(obj)
+    n_bytes = sys.getsizeof(bytes_out)
+    with open(filepath, 'wb') as f_out:
+        for idx in range(0, n_bytes, max_bytes):
+            f_out.write(bytes_out[idx:idx + max_bytes])
 def pickle_load_large_file(filepath):
     max_bytes = 2**31 - 1
     input_size = os.path.getsize(filepath)
@@ -117,6 +124,7 @@ class LookupsInferenceOnly_UMLS:
                                            load_qcode_to_title=return_titles
                                            )
         resource_to_file_path = resource_manager.get_UMLS_data_files()
+        print(resource_to_file_path)
         self.resource_to_file_path = resource_to_file_path
 
         if not self.use_precomputed_description_embeddings:
@@ -127,6 +135,7 @@ class LookupsInferenceOnly_UMLS:
             # TODO: convert to numpy memmap to save space during training with multiple workers
             self.descriptions_tns = None
         self.umlsID_to_idx: Dict[str, int] = load_umlsID_to_idx(resource_to_file_path["qcode_to_idx"])
+
         self.index_path = resource_to_file_path["sapbert_index_path"]
         self.model_dir = resource_to_file_path["sapbert_model"]
         if return_titles:
