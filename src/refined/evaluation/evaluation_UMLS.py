86c2b7b45f5714e4ba76ada0697ec031fad983e2
JerryZeyu <zzy19951013@gmail.com>
2023-02-08 14:42:16 -0700

    update codes

--- src/refined/evaluation/evaluation_UMLS.py
+++ src/refined/evaluation/evaluation_UMLS.py
@@ -25,6 +25,7 @@ def process_annotated_document(
     if force_prediction:
         assert ed_threshold == 0.0, "ed_threshold must be set to 0 to force predictions"
     gold_spans = set()
+    gold_spans_list = []
     gold_entity_in_cands = 0
     all_spans_num = 0
     for span in doc.spans:
@@ -33,11 +34,13 @@ def process_annotated_document(
                 or span.gold_entity.umls_entity_id == "C0"):
             continue
         all_spans_num += 1
+        gold_spans_list.append((span.text, span.start, span.gold_entity.umls_entity_id))
         gold_spans.add((span.text, span.start, span.gold_entity.umls_entity_id))
         if span.gold_entity.umls_entity_id in {umlsID for umlsID, _ in span.candidate_entities}:
             gold_entity_in_cands += 1
     #print("gold_entity_in_cands: ", gold_entity_in_cands)
     #print("el: ", el)
+    assert len(gold_spans_list) == len(gold_spans)
     print("***all_spans_num***: ", all_spans_num)
     # optionally filter NIL gold spans
     # nil_spans is a set of mention spans that are annotated as mentions in the dataset but are not linked to a KB
