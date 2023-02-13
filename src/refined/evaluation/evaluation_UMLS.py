634e2e7b21f8b07702387d26878bf29f202690b4
JerryZeyu <zzy19951013@gmail.com>
2023-02-13 13:14:01 -0700

    update codes

--- src/refined/evaluation/evaluation_UMLS.py
+++ src/refined/evaluation/evaluation_UMLS.py
@@ -79,7 +79,9 @@ def process_annotated_document(
                 umlsID = span.top_k_predicted_entities[1][0].umls_entity_id
         #print("span text: ", span.text)
         #print("span start: ", span.start)
-        #print("span umlsID: ", umlsID)
+        print("span candidates: ", span.candidate_entities)
+        print("span umlsID: ", umlsID)
+        print("***********************")
         pred_spans.add((span.text, span.start, umlsID))
 
     pred_spans = {(text, start, umlsID) for text, start, umlsID in pred_spans if umlsID != "C0"}
